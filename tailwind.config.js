a6e87c85932f7a1fe51a7a499de099f63c4a0311
dinhvanphong <dinhvanphongv4@gmail.com>
2024-01-30 15:24:58 +0700

    update layout reponsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ export default {
   theme: {
     extend: {},
     screens: {
-      sm: "640px",
+      sm: "576px",
       // => @media (min-width: 640px) { ... }
       md: "768px",
       // => @media (min-width: 768px) { ... }
-      lg: "1023px",
+      lg: "992px",
       // => @media (min-width: 1024px) { ... }
-      xl: "1280px",
+      xl: "1200px",
       // => @media (min-width: 1280px) { ... }
 
       // "2xl": "1536px",
